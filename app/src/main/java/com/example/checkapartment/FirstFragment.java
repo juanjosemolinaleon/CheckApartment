9884632a848121ef0a21b3aa4f3b13f76bec95fe
juanjosemolinaleon <molinaleon@gmail.com>
2020-08-24 22:58:03 -0400

    RecyclerView

--- app/src/main/java/com/example/checkapartment/FirstFragment.java
+++ app/src/main/java/com/example/checkapartment/FirstFragment.java
@@ -11,6 +11,8 @@ import androidx.navigation.fragment.NavHostFragment;
 
 public class FirstFragment extends Fragment {
 
+
+
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
--- app/src/main/java/com/example/checkapartment/SecondFragment.java
+++ app/src/main/java/com/example/checkapartment/SecondFragment.java
@@ -13,6 +13,75 @@ import java.util.List;
 
 public class SecondFragment extends Fragment {
 
+    public class Apartment {
+
+        private String buildingName;  // Nombre del edificio
+        private String projectName;   // Nombre del Proyecto
+        private String unitId;        // Numero de unidad o departamento
+        private String address;       // Dirección
+        private String urlImageBuilding;  // url de imagen del edificio
+
+        public Apartment(String buildingName, String projectName, String unitId,
+                         String address, String urlImageBuilding) {
+            this.buildingName = buildingName;
+            this.projectName = projectName;
+            this.unitId = unitId;
+            this.address = address;
+            this.urlImageBuilding = urlImageBuilding;
+        }
+
+        public String getBuildingName() {
+            return buildingName;
+        }
+
+        public void setBuildingName(String buildingName) {
+            this.buildingName = buildingName;
+        }
+
+        public String getProjectName() {
+            return projectName;
+        }
+
+        public void setProjectName(String projectName) {
+            this.projectName = projectName;
+        }
+
+        public String getUnitId() {
+            return unitId;
+        }
+
+        public void setUnitId(String unitId) {
+            this.unitId = unitId;
+        }
+
+        public String getAddress() {
+            return address;
+        }
+
+        public void setAddress(String address) {
+            this.address = address;
+        }
+
+        public String getUrlImageBuilding() {
+            return urlImageBuilding;
+        }
+
+        public void setUrlImageBuilding(String urlImageBuilding) {
+            this.urlImageBuilding = urlImageBuilding;
+        }
+
+        @Override
+        public String toString() {
+            return "Apartment{" +
+                    "buildingName='" + buildingName + '\'' +
+                    ", projectName='" + projectName + '\'' +
+                    ", unitId='" + unitId + '\'' +
+                    ", address='" + address + '\'' +
+                    ", urlImageBuilding='" + urlImageBuilding + '\'' +
+                    '}';
+        }
+    }
+
     public static List<Apartment> apartmentList() {
         List<Apartment> apartmentList = new ArrayList<>();
         Apartment apartment01 = new Apartment("Edificio Almendro","Torre 1", "depto 2002", "san jose 345, La florida , Santiago", "https://unsplash.com/photos/Ub9LkIWxyec/download?force=true&w=640");
@@ -65,7 +134,7 @@ public class SecondFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 NavHostFragment.findNavController(SecondFragment.this)
-                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
+                        .navigate(R.id.action_SecondFragment_to_thirdFragment);
             }
         });
     }
