12e14383663c434958b737eacaa04686de8bc95d
juanjosemolinaleon <molinaleon@gmail.com>
2020-08-26 22:26:01 -0400

    Segundo_Dia_03

--- app/src/main/java/com/example/checkapartment/FirstFragment.java
+++ app/src/main/java/com/example/checkapartment/FirstFragment.java
@@ -1,9 +1,13 @@
 package com.example.checkapartment;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -11,9 +15,16 @@ import androidx.navigation.fragment.NavHostFragment;
 
 public class FirstFragment extends Fragment {
 
+    private EditText code;
+    private Button click;
+
     public void validaPass (String pass){
+        Log.d("TAG",pass);
         if (pass.equals("123Pass")){
-            
+            NavHostFragment.findNavController(FirstFragment.this)
+                    .navigate(R.id.action_FirstFragment_to_SecondFragment);
+        } else {
+
         }
 
     }
@@ -22,20 +33,24 @@ public class FirstFragment extends Fragment {
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
+
     ) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false);
+        View view = inflater.inflate(R.layout.fragment_first, container, false);
+        code = view.findViewById(R.id.textView);
+        click = view.findViewById(R.id.button_first1);
+        click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(), "ACCEDIENDO", Toast.LENGTH_SHORT).show();
+                validaPass(code.getText().toString());
+            }
+        });
+        return view;
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        view.findViewById(R.id.button_first).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
-            }
-        });
-    }
+     }
 }
